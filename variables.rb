07bdf861f95cc729b917bf07b8b628805a06d5e0
Michael Abbott <michael@mjabbottdesign.com>
2021-04-02 18:45:17 -0700

    Assign, modify and manipulate variables

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Rogue One: A Star Wars Story";
+is_entertaining = true;
+movie = "Star Wars: Episode IV - A New Hope";
+num_4 = 4;
+num_6 = 6;
+product = num_6 * num_4;
+favorite_foods = ["pizza", "cheese burger", "chocolate chip cookie"];
+favorite_foods.drop(1);
+movie.length;
