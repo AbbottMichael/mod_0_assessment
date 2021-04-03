bbd508d3936479c4a74cbdf3e6d315b781780036
Michael Abbott <michael@mjabbottdesign.com>
2021-04-02 19:15:12 -0700

    Model developer michael abbott

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer_michael_abbott = {
+  "computer_owned" => "Macbook Pro M1",
+  "student_at" => "Turing",
+  "height_ft" => 6,
+  "number_of_legs" => 2,
+  "is_student" => true,
+  "is_backend" => true,
+  "mod_0_progress" => ["session 1", "session 2", "session 3", "session 4", "session 5"],
+  "turing_payments_made" => [1200, 18300]
+}
