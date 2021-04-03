613cb13e52bafe07e2795c1b1fa3aacf21e951ef
Michael Abbott <michael@mjabbottdesign.com>
2021-04-02 19:04:53 -0700

    Add dinosaur triceratops object

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur_triceratops = {
+  "eaten_by" => "Tyrannosaurus",
+  "extinct_in" => "Cretaceous-Paleogene extinction event",
+  "height_average_ft" => 9.65,
+  "mass_average_lbs" => 19500,
+  "is_cool" => true,
+  "is_a_dog" => false,
+  "eats" => ["cycad", "palm trees"],
+  "fossils" => ["Yoshi's Trike", "AMNH 5039", "Kelsey"]
+}
